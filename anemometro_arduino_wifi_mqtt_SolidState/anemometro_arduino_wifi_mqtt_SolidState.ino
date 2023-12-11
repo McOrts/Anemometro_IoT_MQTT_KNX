348e991f1ce1bd6f9cbdc8691693721977171748
Carlos Orts <mcorts69@gmail.com>
2023-12-11 07:30:21 +0100

    SolidState v

--- anemometro_arduino_wifi_mqtt_SolidState/anemometro_arduino_wifi_mqtt_SolidState.ino
+++ anemometro_arduino_wifi_mqtt_SolidState/anemometro_arduino_wifi_mqtt_SolidState.ino
@@ -1,5 +1,5 @@
 /*****************************************************
- * Date: abril 2022
+ * Date: noviembre 2023
  * Written by: McOrts
  * 
  * ***************************************************/
@@ -8,10 +8,22 @@
 #include "settings.h"
 
 unsigned long int temp1;
+
+/* Configuración lectura sensor */
+unsigned int PWM;
+float WSPE;
+float TEMP;
+unsigned int CRC;
 String trama;
 
-/* Configuración sensor */
-unsigned int WindSpeed ;              
+/* Configuración cálculos */
+unsigned int CountRead;
+float temperature;
+float wind;
+float wind_avg;
+float wind_peak;
+float wind_min;
+float wind_sum;
 
 /* Configuración cliente WiFi */
 WiFiClient espClient;
@@ -34,6 +46,22 @@ void setup() {
   setup_wifi();
   clientMqtt.setServer(mqtt_server, mqtt_port);
   clientMqtt.setCallback(callback);
+
+  /* Inicializa variables cálculos */
+  CountRead = 0;
+  wind = 0.0;
+  wind_avg = 0.0;
+  wind_peak = 0.0;
+  wind_min = 0.0;
+  wind_sum = 0.0;
+}
+
+String IpAddress2String(const IPAddress& ipAddress)
+{
+  return String(ipAddress[0]) + String(".") +\
+  String(ipAddress[1]) + String(".") +\
+  String(ipAddress[2]) + String(".") +\
+  String(ipAddress[3]); 
 }
 
 void setup_wifi() {
@@ -54,11 +82,13 @@ void setup_wifi() {
     Serial.print(".");
   }
 
+  IP = IpAddress2String(WiFi.localIP());
   Serial.println("");
   Serial.println("[WIFI]WiFi conectada");
   Serial.print("[WIFI]IP: ");
-  Serial.print(WiFi.localIP());
+  Serial.print(IP);
   Serial.println("");
+
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -88,7 +118,6 @@ void reconnect() {
       Serial.println("[MQTT]Conectado al servidor MQTT");
       // Once connected, publish an announcement...
       clientMqtt.publish(mqtt_sub_topic_healthcheck, "starting");
-       clientMqtt.publish(mqtt_sub_topic_ip, IP.c_str());
       // ... and subscribe
       clientMqtt.subscribe(mqtt_sub_topic_operation);
     } else {
@@ -101,71 +130,107 @@ void reconnect() {
   }
 }
 
+void clearSerialBuffer() {
+  while (Serial.available() > 0) {
+    Serial.read(); // Read and discard the incoming byte
+  }
+}
 
 void loop() {
-  if (!clientMqtt.connected()) {
+  
+   if (!clientMqtt.connected()) {
     reconnect();
   }
   clientMqtt.loop();
+  
+  if (Serial.available() > 0) {
+    trama = Serial.readStringUntil('\n'); // Lee la trama hasta encontrar un salto de línea
+    //Serial.println(trama);
 
-  delay (2000);
+    // Divide la trama en sus componentes separados por espacios
+    int valores[4];
+    int contador = 0;
+    char *ptr = strtok((char *)trama.c_str(), " ");
+    while (ptr != NULL && contador < 4) {
+      valores[contador] = atoi(ptr);
+      ptr = strtok(NULL, " ");
+      contador++;
+    }
 
-  trama = Serial.readStringUntil('\n'); // Lee la trama hasta encontrar un salto de línea
-  Serial.println(trama);
+    // Asegúrate de que se hayan encontrado los 4 valores esperados
+    if (contador == 4) {
+      PWM = valores[0];
+      WSPE = valores[1];
+      TEMP = valores[2];
+      CRC = valores[3];
 
-  // Divide la trama en sus componentes separados por espacios
-  int valores[4];
-  int contador = 0;
-  char *ptr = strtok((char *)trama.c_str(), " ");
-  while (ptr != NULL && contador < 4) {
-    valores[contador] = atoi(ptr);
-    ptr = strtok(NULL, " ");
-    contador++;
+      wind = WSPE / 100;
+      temperature = TEMP / 100;
+
+      // Cáculos estadísticos
+      wind_sum += wind;
+      CountRead ++;
+
+      if (wind > wind_peak) {
+        wind_peak = wind;
+        Serial.print("wind peak: ");
+        Serial.println(wind_peak);
+       }
+      if (wind < wind_min) {
+        wind_min = wind;
+        Serial.print("wind min: ");
+        Serial.println(wind_min);
+      }
+      
+    } else {
+      Serial.println("Error: Trama de datos incorrecta");
+      clientMqtt.publish(mqtt_sub_topic_healthcheck, "Frame error");
+      clearSerialBuffer();
+    }
   }
 
-  // Asegúrate de que se hayan encontrado los 4 valores esperados
-  if (contador == 4) {
-    int PWM = valores[0];
-    int WSPE = valores[1];
-    int TEMP = valores[2];
-    int CRC = valores[3];
-
-    // Ahora puedes utilizar los valores PWM, WSPE, TEMP y CRC en tu código
-    Serial.print("PWM = ");
-    Serial.println(PWM);
-    Serial.print("WSPE = ");
-    Serial.println(WSPE);
-    Serial.print("TEMP = ");
-    Serial.println(TEMP);
-    Serial.print("CRC = ");
-    Serial.println(CRC);
-  } else {
-    Serial.println("Error: Trama de datos incorrecta");
-  }
-
-  /*  
   if (millis()-temp1>update_time_sensors) {
     temp1=millis();
-    
-    // Lectura del puerto analógico y traducción del voltaje a velocidad  
-    WindSpeed = 6 * analogRead(A0) * (5.0 / 1023.0);
-
-    Serial.print("Velocidad del viento: ");
-    Serial.print(WindSpeed);
-    Serial.println(" m/s");
       
+    wind_avg = wind_sum / CountRead;
+
+    // Lecturas
+    Serial.print("Measurements = ");
+    Serial.println(CountRead);
+    Serial.print("Speed = ");
+    Serial.println(wind_avg);
+    Serial.print("Speed peak = ");
+    Serial.println(wind_peak);
+    Serial.print("Speed minimun = ");
+    Serial.println(wind_min);
+    Serial.print("TEMP = ");
+    Serial.println(temperature);
+
     // Envía la lectura por MQTT
-    snprintf (msg, 10, "%6i", WindSpeed);
-    Serial.print("[MQTT] Sending data: ");
-    Serial.println(msg);
-    clientMqtt.publish(mqtt_pub_topic_windspeed, msg);   
+    Serial.println("[MQTT] Sending data... ");
+    snprintf (msg, 10, "%.2f", wind_avg);
+    clientMqtt.publish(mqtt_pub_topic_windspeed, msg);  
+    snprintf (msg, 10, "%.2f", wind_peak);
+    clientMqtt.publish(mqtt_pub_topic_windspeedpeak, msg);  
+    snprintf (msg, 10, "%.2f", wind_min);
+    clientMqtt.publish(mqtt_pub_topic_windspeedmin, msg);  
+    snprintf (msg, 10, "%.2f", temperature);
+    clientMqtt.publish(mqtt_pub_topic_temperature, msg);  
+    clientMqtt.publish(mqtt_sub_topic_ip, IP.c_str());
+    clientMqtt.publish(mqtt_sub_topic_cp, "07014");
     delay (1000);
-    if (WindSpeed==0) {
+    if (WSPE==0) {
       digitalWrite(LED_BUILTIN, HIGH);       
     } else {
       digitalWrite(LED_BUILTIN, LOW);
     }
+
+    wind_peak = 0;
+    wind_min = 1000;
+    wind_sum = 0;
+    CountRead = 0;
+
   }
-  */      
+
 }
 
